9be962580fc9bdac093202a3e8607e29e3b2f886
Atindra Kumar Singh <atindra.singh86@gmail.com>
2023-02-05 15:05:58 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// THis is some comment
+
+for(let index = 0; index < array.length; index++) {
+
+
+
+
+
+
+  // the purpose of this variable
+  const element = array[index];
+
+
+// The purpose of this variable  
+  let a;
+  let b;
+  let c; 
+
+
+
+  
+}
+
+// the purpose of this variable
+
+let a;
+let b;
+let c;
